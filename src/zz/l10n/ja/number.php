e236867e8f28f03f0b89a537a44c41e51bc54c35
tatsuya nonaka <nonaka.tatsuya@gmail.com>
2012-08-09 00:44:48 +0900

    返り値を文字列に変更

--- src/zz/l10n/ja/number.php
+++ src/zz/l10n/ja/number.php
@@ -7,13 +7,28 @@ namespace zz\l10n\ja;
  */
 class number {
     static protected $_charset = 'UTF-8';
-// 数漢字-数値マップ
+
+    // 数値マップ
+    // 全角 -> アラビア
     static protected $_num_zenkaku = array(
         '０' => 0, '１' => 1, '２' => 2, '３' => 3, '４' => 4, '５' => 5, '６' => 6, '７' => 7, '８' => 8, '９' => 9,
+
     );
 
+
+    // 数漢字 -> アラビア
     static protected $_num_kanji = array(
-        '〇' => 0, '一' => 1, '二' => 2, '三' => 3, '四' => 4, '五' => 5, '六' => 6, '七' => 7, '八' => 8, '九' => 9,
+        '〇' => 0, '一' => 1, '二' => 2, '三' => 3, '四' => 4, '五' => 5, '六' => 6, '七' => 7, '八' => 8, '九' => 9, // 現在
+    );
+
+    // 数漢字 -> アラビア
+    static protected $_num_kanji_alias = array(
+        '零' => 0, // 別名
+        '弌' => 1, '弍' => 2, '弎' => 3, //古字
+        '肆' => 4, '伍' => 5, '陸' => 6, '漆' => 7, '柒' => 7, '質' => 7, '捌' => 8, '玖' => 9, // 旧字体
+        '佰' => '百', '陌' => '百', '仟' => '千', '仟' => '千', '萬' => '万', // 旧字体
+        '壱' => 1, '弐' => 2, '参' => 3, '拾' => '十', '廿' => '二十', '卅' => '三十', '丗' => '三十', // 大字
+        '壹' => 1, '貳' => 2, '參' => 3, // 大字(旧字体)
     );
 
     static protected $_lowerDigits = array(
@@ -21,7 +36,23 @@ class number {
     );
 
     static protected $_upperDigits = array(
-        '万' => 4, '億' => 8, '兆' => 12, '京' => 16,
+        '万' => 4,
+        '億' => 8,
+        '兆' => 12,
+        '京' => 16,
+        '垓' => 20,
+        '𥝱' => 24,
+        '穣' => 28,
+        '溝' => 32,
+        '澗' => 36,
+        '正' => 40,
+        '載' => 44,
+        '極' => 48,
+        '恒河沙' => 52,
+        '阿僧祗' => 56,
+        '那由他' => 60,
+        '不可思議' => 64,
+        '無量大数' => 68,
     );
 
     /**
@@ -48,14 +79,14 @@ class number {
      * arabicメソッドのエイリアス
      * @static
      * @param $noarabic
-     * @return int|mixed|string
+     * @return string
      */
     static function arabic($noarabic) {
         return static::knum2arabic($noarabic);
     }
 
     /**
-     * 10000以下の漢数字をアラビア数字に変換します。
+     * 10000未満の漢数字をアラビア数字に変換します。
      * @static
      * @param $kanji
      * @return int
@@ -68,7 +99,7 @@ class number {
         for ($i = 0, $len = mb_strlen($kanji, static::$_charset); $i < $len; $i++) {
             $ch = mb_substr($kanji, $i, 1, static::$_charset);
             if (isset($lowerDigits[$ch])) {
-                $ival += $lnum ? (int)$lnum * $lowerDigits[$ch] : $lowerDigits[$ch];
+                $ival += $lnum ? $lnum * $lowerDigits[$ch] : $lowerDigits[$ch];
                 $lnum = '';
             } else {
                 $lnum .= $ch;
@@ -85,13 +116,12 @@ class number {
      * 漢数字をアラビア数字に変換します
      * @static
      * @param $kanji
-     * @return int|mixed|string
-     * @throws \RangeException
+     * @return string
      * @throws \UnexpectedValueException
      */
     protected static function knum2arabic($kanji) {
         if ($kanji === null || $kanji === '') {
-            throw new \UnexpectedValueException("empty given");
+            throw new \UnexpectedValueException("empty string or null given");
         }
         $kanji = static::simpleKnum2arabic($kanji);
 
@@ -99,11 +129,11 @@ class number {
             return $kanji;
         }
         // 上桁字-倍率マップ
-        $upperDigits = static::powDex(static::$_upperDigits);
+        $upperDigits = static::$_upperDigits;
 
-        $pattern_separator = join('', array_keys($upperDigits));
+        $pattern_separator = join('|', array_keys($upperDigits));
         $pattern_under_1000 = join('', array_keys(static::$_lowerDigits));
-        $pattern = '/([' . $pattern_under_1000 . '0-9]+)([' . $pattern_separator . ']?)/u'; // 数漢字として扱う文字
+        $pattern = '/([' . $pattern_under_1000 . '0-9]+)((' . $pattern_separator . ')?)/u'; // 数漢字として扱う文字
 
         $parts = array();
         while (preg_match($pattern, $kanji, $matches)) {
@@ -115,8 +145,11 @@ class number {
             throw new \UnexpectedValueException("Can not convert to arabic in `{$kanji}`");
         }
 
-        $ival = 0;
+        $ival = '0000';
+        $parts = array_reverse($parts);
+
         for ($i = 0, $len = count($parts); $i < $len; $i++) {
+
             list($_number, $_digit) = $parts[$i];
             if (static::isNumeric($_number)) {
                 $ipart = $_number;
@@ -124,20 +157,30 @@ class number {
                 $ipart = static::knum2arabic_10000($_number);
             }
 
+            $ipart = sprintf('%04s', $ipart);
+
             if ($_digit) {
                 $imult = $upperDigits[$_digit];
             } else {
                 $imult = 0;
             }
 
-            $ival += $ipart * ($imult != 0 ? $imult : 1);
+            if ($imult) {
+                if (strlen($ival) !== $imult) {
+                    $ival = sprintf('%0' . $imult . 's', $ival);
+                }
+                $ival = $ipart . $ival;
+            } else {
+                if ($i === 0) {
+                    $ival = $ipart;
+                } else {
+                    $ival .= $ipart;
+                }
+
+            }
         }
 
-        if (is_float($ival)) {
-            throw new \RangeException('exceed `PHP_INT_MAX` = ' . PHP_INT_MAX);
-        }
-
-        return $ival;
+        return ltrim($ival, '0');
     }
 
     /**
@@ -148,8 +191,13 @@ class number {
      */
     public static function simpleKnum2arabic($kanji) {
         $_map = array_merge(static::$_num_zenkaku, static::$_num_kanji);
+        $_map_alias = static::$_num_kanji_alias;
 
         $convertCharLists = array();
+
+        foreach ($_map_alias as $alias => $replace) {
+            $kanji = str_replace($alias,$replace, $kanji);
+        }
 
         for ($i = 0, $len = mb_strlen($kanji, static::$_charset); $i < $len; $i++) {
             $char = mb_substr($kanji, $i, 1, static::$_charset);
--- tests/zz/l10n/ja/numberTest.php
+++ tests/zz/l10n/ja/numberTest.php
@@ -11,41 +11,45 @@ class numberTest extends \PHPUnit_Framework_TestCase {
      */
     public function testArabic($kanji, $number) {
         $actual = \zz\l10n\ja\number::arabic($kanji);
-        $this->assertEquals($number, $actual);
+        $this->assertSame($number, $actual);
     }
 
     public function dataArabic() {
         return array(
-            array('二〇〇八', 2008),
-            array('六千九百三十三万二千三百十四', 69332314),
-            array('二千三百四十五万六千七百八十九', 23456789),
-            array('一億二千三百四十五万六千七百八十九', 123456789),
-            // 32bit PHP_INT_MAX 2147483647
-            array('二十一億四千七百四十八万三千六百四十七', 2147483647),
-            array('２億３３万２３１０', 200332310),
+            array('二〇〇八', '2008'),
             array('二〇一一', '2011'),
+            array('十一', '11'),
+            array('一千', '1000'),
+            array('一万', '10000'),
+            array('一万一', '10001'),
+            array('二十一万一', '210001'),
+            array('一億', '100000000'),
+            array('六千九百三十三万二千三百十四', '69332314'),
+            array('二千三百四十五万六千七百八十九', '23456789'),
+            array('一億二千三百四十五万六千七百八十九', '123456789'),
+            array('２億３３万２３１０', '200332310'),
+            // 32bit PHP_INT_MAX => 2147483647
+            array('二十一億四千七百四十八万三千六百四十七', '2147483647'),
+            array('二一四七四八三六四七', '2147483647'),
+            // 32bit PHP_INT_MAX + 1 => 2147483648
+            array('二十一億四千七百四十八万三千六百四十八', '2147483648'),
+            array('二一四七四八三六四八', '2147483648'),
+            // 64bit PHP_INT_MAX => 9223372036854775807
+            array('九二二三三七二兆三百六十八億五千四百七十七万五千八百七', '9223372036854775807'),
+            // 64bit PHP_INT_MAX => 9223372036854775808
+            array('九二二三三七二兆三百六十八億五千四百七十七万五千八百八', '9223372036854775808'),
+
+            array('一〇〇〇億', '100000000000'),
+            array('五〇〇億', '50000000000'),
+            array('一千億', '100000000000'),
+            array('五百億', '50000000000'),
+
+            array('一無量大数', '100000000000000000000000000000000000000000000000000000000000000000000'),
+
+            array('百弐拾万参千弐百', '1203200'),
         );
     }
 
-    /**
-     *
-     * @dataProvider dataArabic64bit
-     */
-    public function testArabic64bit($kanji, $number) {
-       if (PHP_INT_SIZE <= 4) {
-           $this->markTestSkipped('require 64bit');
-       }
-        $this->testArabic($kanji, $number);
-    }
-
-    public function dataArabic64bit() {
-        return array(
-            array('一〇〇〇億', 100000000000),
-            array('五〇〇億', 50000000000),
-            array('一千億', 100000000000),
-            array('五百億', 50000000000),
-        );
-    }
 
     /**
      * @expectedException UnexpectedValueException
